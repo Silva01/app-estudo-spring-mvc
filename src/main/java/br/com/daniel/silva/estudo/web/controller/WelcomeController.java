be457938e60de27cadbc611831851bab8ddae075
silva <daniel.santos@oi.net.br>
2019-07-23 21:12:19 -0300

    Add new class and methods for Controller learn

---
+++ src/main/java/br/com/daniel/silva/estudo/web/controller/WelcomeController.java
@@ -0,0 +1,22 @@
+package br.com.daniel.silva.estudo.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String welcome() {
+		return "welcome";
+	}
+	
+	@RequestMapping(value = "/teste", method = RequestMethod.GET)
+	public ModelAndView teste() {
+		ModelAndView view = new ModelAndView("welcome");
+		view.addObject("texto", "Obtendo valor de um controller com Spring MVC");
+		return view;
+	}
+}
